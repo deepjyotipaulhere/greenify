de6336e67e8a717dfbb56c81cfe11691f5505e34
Deepjyoti Paul <deepjyotipaul69@gmail.com>
2025-05-09 23:34:59 +0530

    initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,33 @@
+import { useColorScheme } from '@/hooks/useColorScheme';
+import * as eva from '@eva-design/eva';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { ApplicationProvider } from '@ui-kitten/components';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { default as theme } from '../constants/custom-theme.json'; // <-- Import app theme
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+	const colorScheme = useColorScheme();
+	const [loaded] = useFonts({
+		SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+	});
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+	if (!loaded) {
+		// Async font loading only occurs in development.
+		return null;
+	}
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+			<ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
+				<Stack>
+					<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+					<Stack.Screen name="+not-found" />
+				</Stack>
+				<StatusBar style="auto" />
+			</ApplicationProvider>
+		</ThemeProvider>
+	);
 }
